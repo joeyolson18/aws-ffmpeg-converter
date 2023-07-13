fb3ffe521b85b286790e96f7193f102c3a740714
joeyolson18 <olsonjoey83@gmail.com>
2023-07-13 17:51:44 -0600

    v1

--- v1/decode-v1.bash
+++ v1/decode-v1.bash
@@ -22,18 +22,18 @@ do
 
         aws sqs delete-message --queue-url "$queue_url" --receipt-handle "${receipt}"
 
-        object_key=`jq '.Body.Records[0].s3.object.key' <<< "$first_message"`
-        object_key=`sed 's/\"//g' <<< $object_key`
-        object_key_no_path=`sed 's|.*/||' <<< $object_key`
+        object_key_path=`jq '.Body.Records[0].s3.object.key' <<< "$first_message"`
+        object_key_path=`sed 's/\"//g' <<< $object_key_path`
+        object_key=`sed 's|.*/||' <<< $object_key_path`
 
-        aws s3 cp "${bucket_name}uploads/${object_key}" ~/sq.mp4
+        aws s3 cp "${bucket_name}${object_key_path}" ~/sq.mp4
 
         ffmpeg -i ~/sq.mp4 -vf scale=320:240 ~/lq.mp4
 
-        new_path="${bucket_name}${object_key_no_path}"
+        new_path="${bucket_name}${object_key}"
         aws s3 cp ~/lq.mp4 "${new_path}/lq.mp4"
         aws s3 cp ~/sq.mp4 "${new_path}/sq.mp4"
-        aws s3 rm "${bucket_name}${object_key}"
+        aws s3 rm "${bucket_name}${object_key_path}"
 
         rm ~/lq.mp4
         rm ~/sq.mp4
