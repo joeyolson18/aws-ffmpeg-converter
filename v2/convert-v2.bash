359af69bfe14078bcc0abbe2730d7e65d6694e25
joeyolson18 <olsonjoey83@gmail.com>
2023-07-15 18:17:33 -0600

    v1 final

---
+++ v2/convert-v2.bash
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+while true
+do
+    echo "Polling Queue"
+
+    bucket_name="<bucket-name>"
+    queue_url="<queue-name>"
+
+    json=`aws sqs receive-message --queue-url "${queue_url}"`
+    if [ "$json" ]
+    then
+        json=`sed 's/\\\"/\"/g' <<< $json`
+        json=`sed 's/\"{/{/g' <<< $json`
+        json=`sed 's/}\"/}/g' <<< $json`
+        echo "$json"
+
+        first_message=`jq '.Messages[0]' <<< "$json"`
+
+        receipt=`jq '.ReceiptHandle' <<< "$first_message"`
+        receipt=`sed 's/\"//g' <<< $receipt`
+
+        aws sqs delete-message --queue-url "$queue_url" --receipt-handle "${receipt}"
+
+        object_key_path=`jq '.Body.Records[0].s3.object.key' <<< "$first_message"`
+        object_key_path=`sed 's/\"//g' <<< $object_key_path`
+        object_key=`sed 's|.*/||' <<< $object_key_path`
+
+        aws s3 cp "${bucket_name}${object_key_path}" ~/sq.mp4
+
+        ffmpeg -i ~/sq.mp4 -vf scale=320:240 ~/lq.mp4
+
+        new_path="${bucket_name}${object_key}"
+        aws s3 cp ~/lq.mp4 "${new_path}/lq.mp4"
+        aws s3 cp ~/sq.mp4 "${new_path}/sq.mp4"
+        aws s3 rm "${bucket_name}${object_key_path}"
+
+        rm ~/lq.mp4
+        rm ~/sq.mp4
+    fi
+        sleep 10
+done
+
